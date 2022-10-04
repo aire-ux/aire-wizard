367d311e964b4e7b4a2b1f2cab340aeabfad8cd4
josiah <j@groovv.co>
2022-10-04 16:16:20 -0500

    Adding types

---
+++ src/main/java/io/sunshower/aire/components/WizardModelSupport.java
@@ -0,0 +1,16 @@
+package io.sunshower.aire.components;
+
+import com.vaadin.flow.component.Component;
+
+public interface WizardModelSupport<K, T> {
+
+  boolean addModelElement(T value);
+
+  <V extends Component> boolean transitionTo(Class<V> type);
+
+  boolean transitionTo(K key);
+
+  default void onEntered(Wizard<K, T> wizard) {}
+
+  default void onExited(Wizard<K, T> wizard) {}
+}
