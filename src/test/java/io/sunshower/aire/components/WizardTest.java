fc4165245ab41beef2f29ea92cf2790938dd4f43
josiah <j@groovv.co>
2022-10-07 18:40:03 -0500

    Adding test dependencies

--- src/test/java/io/sunshower/aire/components/WizardTest.java
+++ src/test/java/io/sunshower/aire/components/WizardTest.java
@@ -1,3 +1,10 @@
 package io.sunshower.aire.components;
 
+import com.aire.ux.test.AireTest;
+import com.aire.ux.test.spring.EnableSpring;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AireTest
+@EnableSpring
+@SpringBootTest()
 public class WizardTest {}
